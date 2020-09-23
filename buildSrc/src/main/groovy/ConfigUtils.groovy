81e656f9ec14808346352ea8d6bc5b97edc40eac
mny <mny9@outlook.com>
2020-09-23 23:15:32 +0800

    【学习】注释

--- buildSrc/src/main/groovy/ConfigUtils.groovy
+++ buildSrc/src/main/groovy/ConfigUtils.groovy
@@ -4,7 +4,7 @@ import org.gradle.api.ProjectState
 import org.gradle.api.invocation.Gradle
 
 class ConfigUtils {
-
+    // 在 根 build.gradle 上调用，初始化各种依赖项
     static init(Gradle gradle) {
         generateDep(gradle)
         addCommonGradle(gradle)
@@ -17,11 +17,15 @@ class ConfigUtils {
     private static void generateDep(Gradle gradle) {
         def configs = [:]
         for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
+            // 解构赋值 depConfig 的 key 和 值
             def (name, config) = [entry.key, entry.value]
+            // 根据条件判断最终的 dep 是什么
+            // 如果配置了 插件，那么就是插件
             if (config.pluginPath) {
                 config.dep = config.pluginPath
             } else {
                 if (config.useLocal) {
+                    // 结果形如： project ':feature:launcher:feature_launcher_app'
                     config.dep = gradle.rootProject.findProject(config.projectPath)
                 } else {
                     config.dep = config.remotePath
@@ -31,23 +35,28 @@ class ConfigUtils {
         }
         GLog.l("generateDep = ${GLog.object2String(configs)}")
     }
-
+    // 通过这种方式，可以写通用的 apply
     private static addCommonGradle(Gradle gradle) {
         gradle.addProjectEvaluationListener(new ProjectEvaluationListener() {
             @Override
             void beforeEvaluate(Project project) {
                 // 在 project 的 build.gradle 前 do sth.
                 if (project.subprojects.isEmpty()) {
+                    // 表明不是 Project 的 build.gradle
+                    // 而是 module 的 build.gradle
                     if (project.path.contains(":plugin:")) {
+                        // 需要遵守约定，插件都以 plugin 作为包名
                         return
                     }
                     if (project.name.endsWith("_app")) {
                         GLog.l(project.toString() + " applies buildApp.gradle")
+                        // 给 app 设置 buildApp.gradle
                         project.apply {
                             from "${project.rootDir.path}/buildApp.gradle"
                         }
                     } else {
                         GLog.l(project.toString() + " applies buildLib.gradle")
+                        // 其他的 设置 buildLib.gradle
                         project.apply {
                             from "${project.rootDir.path}/buildLib.gradle"
                         }
@@ -61,7 +70,7 @@ class ConfigUtils {
             }
         })
     }
-
+    // 获取所有的 plugin，命名都以 plugin_ 起头
     static getApplyPlugins() {
         def plugins = [:]
         for (Map.Entry<String, DepConfig> entry : Config.depConfig.entrySet()) {
